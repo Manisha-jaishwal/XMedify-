bd9bc0e219db06b37f450ab68d5dcf679f701339
Manisha-jaishwal <manishajaishwal411@gmail.com>
2026-02-01 18:36:34 +0530

    updateee

--- src/components/SearchHospital/SearchHospital.jsx
+++ src/components/SearchHospital/SearchHospital.jsx
@@ -1,61 +1,152 @@
-import { MenuItem, Select, Button, Box } from "@mui/material";
+import {
+  MenuItem,
+  Select,
+  Button,
+  InputAdornment,
+  Box,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export default function SearchHospital() {
-  const [states, setStates] = useState([]);
-  const [cities, setCities] = useState([]);
-  const [state, setState] = useState("");
-  const [city, setCity] = useState("");
+  const [stateList, setStateList] = useState([]);
+  const [cityList, setCityList] = useState([]);
+  const [selection, setSelection] = useState({
+    state: "",
+    city: "",
+  });
 
   const navigate = useNavigate();
 
+  // Load states
   useEffect(() => {
-    axios
-      .get("https://meddata-backend.onrender.com/states")
-      .then((res) => setStates(res.data));
+    const loadStates = async () => {
+      try {
+        const res = await axios.get(
+          "https://meddata-backend.onrender.com/states"
+        );
+        setStateList(res.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    loadStates();
   }, []);
 
+  // Load cities when state changes
   useEffect(() => {
-    if (!state) return;
-    setCity("");
-    axios
-      .get(`https://meddata-backend.onrender.com/cities/${state}`)
-      .then((res) => setCities(res.data));
-  }, [state]);
+    if (!selection.state) return;
+
+    const loadCities = async () => {
+      setCityList([]);
+      setSelection((prev) => ({ ...prev, city: "" }));
+
+      try {
+        const res = await axios.get(
+          `https://meddata-backend.onrender.com/cities/${selection.state}`
+        );
+        setCityList(res.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    loadCities();
+  }, [selection.state]);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setSelection((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (state && city) {
-      navigate(`/search?state=${state}&city=${city}`);
+
+    if (selection.state && selection.city) {
+      navigate(
+        `/search?state=${selection.state}&city=${selection.city}`
+      );
     }
   };
 
   return (
-    <Box component="form" onSubmit={handleSubmit}>
-      
-      <div id="state">
-        <Select value={state} onChange={(e) => setState(e.target.value)} displayEmpty>
-          <MenuItem value="" disabled>State</MenuItem>
-          {states.map((s) => (
-            <MenuItem key={s} value={s}>{s}</MenuItem>
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        display: "flex",
+        gap: 4,
+        flexDirection: { xs: "column", md: "row" },
+      }}
+    >
+      {/* STATE */}
+      <Box id="state" sx={{ width: "100%" }}>
+        <Select
+          fullWidth
+          name="state"
+          value={selection.state}
+          onChange={handleChange}
+          displayEmpty
+          required
+          startAdornment={
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          }
+        >
+          <MenuItem value="" disabled>
+            State
+          </MenuItem>
+          {stateList.map((state) => (
+            <MenuItem key={state} value={state}>
+              {state}
+            </MenuItem>
           ))}
         </Select>
-      </div>
+      </Box>
 
-
-      <div id="city">
-        <Select value={city} onChange={(e) => setCity(e.target.value)} displayEmpty>
-          <MenuItem value="" disabled>City</MenuItem>
-          {cities.map((c) => (
-            <MenuItem key={c} value={c}>{c}</MenuItem>
+      {/* CITY */}
+      <Box id="city" sx={{ width: "100%" }}>
+        <Select
+          fullWidth
+          name="city"
+          value={selection.city}
+          onChange={handleChange}
+          displayEmpty
+          required
+          startAdornment={
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          }
+        >
+          <MenuItem value="" disabled>
+            City
+          </MenuItem>
+          {cityList.map((city) => (
+            <MenuItem key={city} value={city}>
+              {city}
+            </MenuItem>
           ))}
         </Select>
-      </div>
+      </Box>
 
-      
-      <Button type="submit" id="searchBtn">
+      {/* SEARCH BUTTON */}
+      <Button
+        id="search"
+        type="submit"
+        variant="contained"
+        size="large"
+        startIcon={<SearchIcon />}
+        disableElevation
+        sx={{ px: 6 }}
+      >
         Search
       </Button>
     </Box>
